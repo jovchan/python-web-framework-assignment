f821bec7a03751d8c288821494b6703ab05bee10
jovchan <109028241+jovchan@users.noreply.github.com>
2023-03-18 23:38:17 +0700

    Update Tugas2.py

--- Tugas2.py
+++ Tugas2.py
@@ -23,15 +23,3 @@ add_slider = st.sidebar.slider(
     'Select a range of values',
     0.0, 100.0, (25.0, 75.0)
 )
-
-st.subheader('Connection with googlesheet')
-# Read in data from the Google Sheet.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
-def load_data(sheets_url):
-    csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
-    return pd.read_csv(csv_url)
-
-df = load_data(st.secrets["public_gsheets_url"])
-
-st.bar_chart(df)
